3339c1f4859f3561427836107952930632a68f5e
Michael Selutin <selutin1@yahoo.de>
2022-01-12 09:23:38 +0200

    hi

---
+++ week3/day2/dailyChallenge/challenge.js
@@ -0,0 +1,16 @@
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+
+
+fruits.shift();
+
+console.log(fruits);
+
+fruits.sort();
+
+fruits.push("kiwi");
+
+fruits.splice(0, 1, 0);
+
+fruits.reverse()
+
+console.log(fruits);
